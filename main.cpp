e66a15fc461b49e71faee03d48967a33f951bf8f
Daryl Jacura <d.jacura@sky.com>
2021-09-12 10:18:30 +0100

    Added Prop class and header, and created Render function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "Character.h"
+#include "Prop.h"
 
 
 int main()
@@ -15,6 +16,8 @@ int main()
 
     Character knight{windowWidth, windowHeight};
 
+    Prop rock{Vector2{0.f,0.f}, LoadTexture("nature_tileset/Rock.png")};
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
@@ -25,6 +28,9 @@ int main()
 
         // draw the map
         DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
+
+        rock.Render(knight.getWorldPos());
+
         knight.tick(GetFrameTime());
         // Check map bounds
         if (knight.getWorldPos().x < 0.f ||
